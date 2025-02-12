efdce032f724cbb5b544dc1f517b99f2def30bd6
SofiaNords <sofia.k.nordstrom@gmail.com>
2025-02-12 14:57:04 +0100

    Add images to products.js

--- js/products.js
+++ js/products.js
@@ -1,17 +1,17 @@
 const products = [
     {
       name: "Kajak 1 person",
-      image: "https://overlandexperten.com/wp-content/uploads/2023/09/Arcway-Havskajak-Nidingen-Gul-2.webp",
+      image: "../assets/images/kayak-single.webp",
       description: "Detta är en detaljerad beskrivning av Produkt 1. Här kan vi skriva en massa saker om produkten"
     },
     {
       name: "Kajak 3 personer",
-      image: "https://quickbutik.imgix.net/4932Q/products/66c99eab9ad5b.jpeg?w=250&h=250&fit=crop&auto=format",
+      image: "../assets/images/kayak-double.webp",
       description: "Detta är en detaljerad beskrivning av Produkt 2."
     },
     {
       name: "Produkt 3",
-      image: "https://via.placeholder.com/150",
+      image: "../assets/images/sup.webp",
       description: "Detta är en detaljerad beskrivning av Produkt 3."
     }
   ];
